5c6bb14fb10fd866b5c1c423269f217296783afa
Roman Chacin <roman.chacin@correo.unimet.edu.ve>
2023-06-18 18:17:35 -0400

    Casi listo Vehiculo

--- src/main/java/clases/Vehiculo.java
+++ src/main/java/clases/Vehiculo.java
@@ -4,6 +4,8 @@
  */
 package clases;
 import java.util.Random;
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
 /**
  *
  * @author roman
@@ -16,77 +18,120 @@ public class Vehiculo {
     String marca;    //Esto indica si es Bugatti o Lamborghini
     String nombre;
     int id;
-    int prioridad;         //refuerzo sera nivel con valor 4     
-    boolean carroceria_calidad;
-    boolean chasis_calidad;
-    boolean motor_calidad;
-    boolean rueda_calidad;
+    int cola_prioridad;              
+    double calidadRueda;
+    double calidadMotor;
+    double calidadCarroceria;
+    double calidadChasis;  
     boolean puede_pasar;
     
-   
     
-    public Vehiculo(String marca,String nombre,int id,boolean carroceria_calidad,boolean chasis_calidad,boolean motor_calidad,boolean rueda_calidad,boolean puede_pasar) {
+    public Vehiculo(String marca,String nombre,int id) {
         this.contador = 0;
         this.next = null;
-        this.calidad = "Sin calidad asignada";
-        this.prioridad = 4; 
+        this.calidad = "Sin calidad asignada"; 
+        this.puede_pasar = false;
                 
         this.marca = marca;
         this.nombre = nombre;
-        this.id = id;      
-        this.carroceria_calidad = carroceriaCalidad();
-        this.chasis_calidad = chasisCalidad();
-        this.motor_calidad = motorCalidad();
-        this.rueda_calidad = ruedaCalidad();
-        this.puede_pasar = false;
+        this.id = id; 
+        this.calidadRueda = ruedaCalidad();
+        this.calidadMotor = motorCalidad();
+        this.calidadCarroceria = carroceriaCalidad();
+        this.calidadChasis = chasisCalidad();
+        
+        this.cola_prioridad = colaInicial(this.calidadRueda, this.calidadMotor, this.calidadCarroceria, this.calidadChasis);    
     }
     
     public void aumentar_contador() {
         //cada vez que se complete una ronda en la inteligencia artificial.
     }
     
-    private boolean carroceriaCalidad() {
-        Random randomCarroceria = new Random();
-        int calidadCarroceria = (int) (randomCarroceria.nextInt(100) + 1);
-        if (calidadCarroceria <= 60) {
-            return true;
-        } else {
-            return false;
-        }       
+    private double ruedaCalidad() {
+       Random randomRueda = new Random();
+       int nroAleatorioRueda = (int) (randomRueda.nextInt(100) + 1);
+       if (nroAleatorioRueda <= 40) {
+           this.calidadRueda = (double) (25 - 0.08*nroAleatorioRueda);
+           //System.out.println("El numero aleatorio de Rueda es: "+nroAleatorioRueda+" y por ende es calidad");
+           return this.calidadRueda;
+       } else {
+           this.calidadRueda = (double) (14 - 0.05*nroAleatorioRueda);
+           //System.out.println("El numero aleatorio de Rueda es: "+nroAleatorioRueda+" y por ende no es calidad");
+           return this.calidadRueda;
+       }       
     }
     
-    private boolean chasisCalidad() {
-        Random randomChasis = new Random();
-        int calidadChasis = (int) (randomChasis.nextInt(100) + 1);
-        if (calidadChasis <= 70) {
-            return true;
-        } else {
-            return false;
-        }       
-    }
-    
-    private boolean motorCalidad() {
+    private double motorCalidad() {
         Random randomMotor = new Random();
-        int calidadMotor = (int) (randomMotor.nextInt(100) + 1);
-        if (calidadMotor <= 50) {
-            return true;
+        int nroAleatorioMotor = (int) (randomMotor.nextInt(100) + 1);
+        if (nroAleatorioMotor <= 50) {
+            this.calidadMotor = (double) (24 - 0.06*nroAleatorioMotor);
+            //System.out.println("El numero aleatorio de Motor es: "+nroAleatorioMotor+ " y por ende es calidad");
+            return this.calidadMotor;
         } else {
-            return false;
+            this.calidadMotor = (double) (14 - 0.06*nroAleatorioMotor);
+            //System.out.println("El numero aleatorio de Motor es: "+nroAleatorioMotor+ " y por ende no es calidad");
+            return this.calidadMotor;
+        }       
+    }
+       
+    private double carroceriaCalidad() {
+        Random randomCarroceria = new Random();
+        int nroAleatorioCarroceria = (int) (randomCarroceria.nextInt(100) + 1);
+        if (nroAleatorioCarroceria <= 60) {
+            this.calidadCarroceria = (double) (23 - 0.05*nroAleatorioCarroceria);
+            //System.out.println("El numero aleatorio de Carroceria es: "+nroAleatorioCarroceria+ " y por ende es calidad");
+            return this.calidadCarroceria;
+        } else {
+            this.calidadCarroceria = (double) (15 - 0.08*nroAleatorioCarroceria);
+            //System.out.println("El numero aleatorio de Carroceria es: "+nroAleatorioCarroceria+ " y por ende no es calidad");
+            return this.calidadCarroceria;
         }       
     }
     
-    private boolean ruedaCalidad() {
-        Random randomRueda = new Random();
-        int calidadRueda = (int) (randomRueda.nextInt(100) + 1);
-        if (calidadRueda <= 40) {
-            return true;
+    private double chasisCalidad() {
+        Random randomChasis = new Random();
+        int nroAleatorioChasis = (int) (randomChasis.nextInt(100) + 1);
+        if (nroAleatorioChasis <= 70) {
+            this.calidadChasis = (double) (22 - 0.04*nroAleatorioChasis);
+            //System.out.println("El numero aleatorio de Chasis es: "+nroAleatorioChasis + " y por ende es calidad");
+            return this.calidadChasis;
         } else {
-            return false;
+            this.calidadChasis = (double) (16 - 0.1*nroAleatorioChasis);
+            //System.out.println("El numero aleatorio de Chasis es: "+nroAleatorioChasis + "y por ende no es calidad");
+            return this.calidadChasis;
         }       
     }
     
-//    public String info(String marca,String nombre,int id,String calidad) {
-//        return ("El vehiculo: "+nombre+" de la marca: "+marca+" de id: "+id"y calidad: "+calidad+" esta compitiendo");
-//    }
+    private int colaInicial(double ruedas,double motor,double carroceria,double chasis) {
+        double determinanteColaInicial = 0.8185*ruedas + 0.7727*motor + 0.7270*carroceria + 0.6818*chasis;
+        //System.out.println(determinanteColaInicial);
+        DecimalFormat df = new DecimalFormat("#.##");
+        df.setRoundingMode(RoundingMode.HALF_UP);
+        String numeroRedondeado = df.format(determinanteColaInicial);
+        numeroRedondeado = numeroRedondeado.replace(',','.');
+        determinanteColaInicial = Double.parseDouble(numeroRedondeado);
+        //System.out.println(determinanteColaInicial);
+        if (determinanteColaInicial < 40) {
+            this.cola_prioridad = 3;
+            return this.cola_prioridad;
+        } else if (determinanteColaInicial >= 40 && determinanteColaInicial < 55) {
+            this.cola_prioridad = 2;
+            return this.cola_prioridad;
+        } else {
+            this.cola_prioridad = 1;
+            return this.cola_prioridad;
+        }
+    }
     
+ 
+    
+    public void info1() {
+        System.out.println("El vehiculo: "+this.nombre+" de la marca: "+this.marca+" de id: "+this.id+ " esta compitiendo");
+    }
+    
+    public void info2() {
+        System.out.println("Ruedas: "+this.calidadRueda+" Motor: "+this.calidadMotor+" Carroceria: "
+                +this.calidadCarroceria+" Chasis: "+this.calidadChasis+" Esta en la cola: "+this.cola_prioridad);
+    }
 }
